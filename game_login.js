cd9185c6b3a1a097e530c55b783e00759c84f2db
osschief18gmailcom <83204555+osschief18gmailcom@users.noreply.github.com>
2021-07-07 10:38:22 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+
+function add_user(){
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+
+localStorage.setItem("player1_name_input",player1_name);
+localStorage.setItem("player2_name_input",player2_name);
+
+window.location="/Game Page/game_page.html";
+}
